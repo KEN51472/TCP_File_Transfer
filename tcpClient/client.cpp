ff4302fad6abcb5d63f868744ce11d22d15314e5
yukuan <kenyu.99711@gmail.com>
2022-05-21 15:57:06 +0800

    修改格式,对函数返回值进行判断

--- tcpClient/client.cpp
+++ tcpClient/client.cpp
@@ -9,86 +9,81 @@
 
 #define SERV_PORT 9877
 
-int main(int argc, char **argv)
-{
-    int sockfd;
+int main(int argc, char **argv) {
+    int sockfd,connfd;
     struct sockaddr_in servaddr;
-
-    if (argc != 2)
-    {
-        exit(0);
+    if (argc != 2) {
+        return 0;
     }
-
-    //创建套接口，初始化套接口地址结构
+    //Create a socket, initialize the socket address structure
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1) {
+        printf("Create socket error...\n");
+        return -1;
+    }
+    printf("Create socket success...\n");
     memset(&servaddr, 0,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
-    inet_pton(AF_INET, argv[1], &servaddr.sin_addr); //转换由argv指针所指的字符串，并通过servaddr.sin_addr指针存放二进制结果，成功返回1失败返回0
-
-    //与服务器连接
-    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0; //与服务器建立连接
-
-    char file_path[128] = {0};  //文件路径
-    char file_info[2048] = {0}; //文件信息
-
-    char buf[1024] = {0};
-
-    //获取用户输入的文件路径
-    printf("请输入需要传输文件路径: ");
-    scanf("%s", file_path);
-
-    //从文件路径中获取文件名，如"test/a.txt" ==> "a.txt"
-    char file_name[128] = {0};
-    strncpy(file_name, basename(file_path), sizeof(file_name));
-
-    //打开文件
-    int fd = open(file_path, O_RDWR);
-    if (fd == -1)
-    {
-        printf("打开文件[%s]失败", file_path);
+    //Convert the string pointed to by the argv pointer, and store the binary result through the servaddr.sin_addr pointer, return 1 if successful, and return 0 if it fails.
+    inet_pton(AF_INET, argv[1], &servaddr.sin_addr); 
+    //Connect to server
+    connfd = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0; 
+    if(connfd == -1) {
+        printf("Connect error...\n");
         return -1;
     }
 
-    //计算文件大小
+    char file_path[128] = {0};  
+    char file_info[2048] = {0}; 
+    char buf[1024] = {0};
+    printf("Please enter the path of the file to be transferred: ");
+    scanf("%s", file_path);
+    //Get filename from filepath
+    char file_name[128] = {0};
+    strncpy(file_name, basename(file_path), sizeof(file_name));
+    //Open the file
+    int fd = open(file_path, O_RDWR);
+    if (fd == -1) {
+        printf("Open [%s] error...\n", file_path);
+        return -1;
+    }
+    //Calculate file size
     int len = lseek(fd, 0, SEEK_END);
-
-    //文件光标偏移到文件开始位置
+    //Offset the file cursor to the beginning of the file
     lseek(fd, 0, SEEK_SET);
-
-    //将文件大小和文件名存放到file_info
+    if(sizeof(len)>sizeof(file_info)) {
+        printf("File is too large to transfer...\n");
+        return -1;
+    }
+    //Store file size and file name in file_info
     sprintf(file_info, "%d", len);
     strcpy(file_info + 16, file_name);
-
-    // 将需要上传的文件名告诉对方
-    write(sockfd, file_info, 144);
-
-    int send_len = 0; //记录发送了多少字节
-
-    while (1)
-    {
+    // Tell server the name of the file to be uploaded
+    int writefd = write(sockfd, file_info, 144);
+    if(writefd == -1) {
+        printf("using function write error...\n");
+        return -1;
+    }
+    int sent = 0; 
+    while (1) {
         memset(buf, 0,sizeof(buf));
-        //读取数据
+        //Read data
         int ret = read(fd, buf, sizeof(buf));
-        if (ret <= 0)
-        {
-            printf("传输文件[%s] 成功...\n", file_name);
+        if (ret <= 0) {
+            printf("Transfer [%s] success...\n", file_name);
             break;
         }
-
-        //发送数据
-        write(sockfd, buf, ret);
-
-        send_len += ret; //统计发送了多少字节
-
-        //上传文件的百分比
-        printf("上传中 %.2f%%\n", (float)send_len / len * 100);
+        //Send data
+        int writefd = write(sockfd, buf, ret);
+        if(writefd == -1) {
+        printf("Using function write error...\n");
+        return -1;
+    }   
+        sent += ret; 
+        printf("Uploading ... %.2f%%\n", (float)sent / len * 100);
     }
-
-    // 关闭文件
+    
     close(fd);
-
-    // str_cli(stdin, sockfd); 
-
-    exit(0);
+    return 0;
 }
--- tcpServer/server.cpp
+++ tcpServer/server.cpp
@@ -10,77 +10,88 @@
 #define LISTENQ 1024
 #define	SA	struct sockaddr
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int listenfd, connfd;
     pid_t childpid;
     socklen_t clilen;
     struct sockaddr_in cliaddr, servaddr;
-
-    //创建套接口，捆绑服务器的端口
-    listenfd = socket(AF_INET, SOCK_STREAM, 0); //创建一个套接口
-    printf("创建套接口成功...\n");
-    memset(&servaddr, 0,sizeof(servaddr)); //将内存块的前sizeof(servaddr)个字节清零
+    //Create a socket and bind the port of the server
+    listenfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if(listenfd == -1) {
+        printf("Create socket error...\n");
+        return -1;
+    }
+    printf("Create socket success...\n");
+    memset(&servaddr, 0,sizeof(servaddr)); 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERV_PORT);
-    bind((listenfd), (SA *)&servaddr, sizeof(servaddr)); //套接口绑定提供服务的端口
-    printf("套接口绑定端口成功...\n");
-    listen(listenfd, LISTENQ); //指示套接口进入监听连接请求状态
-    printf("套接口进入监听成功...\n");
-    printf("等待完成客户连接...\n");
-    for (;;)
-    { //等待完成客户连接
+    bind((listenfd), (SA *)&servaddr, sizeof(servaddr)); 
+    printf("Binding the port success...\n");
+    listen(listenfd, LISTENQ); 
+    printf("Listening success...\n");
+    printf("Waiting for client connection to complete...\n");
+    for (;;) { 
+        //waiting for connect
         clilen = sizeof(cliaddr);
-        connfd = accept(listenfd, (SA *)&cliaddr, &clilen); //接受连接请求，进入连接状态
-        printf("连接成功...\n");
-
-        //并发服务器
-        if ((childpid = fork()) == 0)
-        {
+        //accept success 
+        connfd = accept(listenfd, (SA *)&cliaddr, &clilen); 
+        if(connfd == -1) {
+            printf("Accept error...\n");
+            return -1;
+        }
+        printf("Connect success...\n");
+        //concurrent server
+        if ((childpid = fork()) == 0) {
             close(listenfd);
-            char file_len[16] = {0};   //文件长度
-            char file_name[128] = {0}; //文件名称
-            char buf[1024] = {0};      //数据缓冲区
+            char file_len[16] = {0};   
+            char file_name[128] = {0}; 
+            //data buffer
+            char buf[1024] = {0};      
             char filepath[2048] = {0};
-            //读取文件大小和文件名称
-            read(connfd, buf, sizeof(buf));                                //将accept中的所有内容读取到buf指针所指的内存中
-            strncpy(file_len, buf, sizeof(file_len));                      //取出文件大小(复制字符串前n个字符)
-            strncpy(file_name, buf + sizeof(file_len), sizeof(file_name)); //取出文件名称
-
-            printf("准备接收文件...... 文件名:[%s] 大小:[%s] \n", file_name, file_len);
-
-            //新的文件名
-            sprintf(buf, "recv-%s", file_name);                   //将格式化数据写入字符串
-            sprintf(filepath, "/home/code/tcp_download/%s", buf); //指定文件接收位置
-            //创建新文件
+            //Read file size and file name
+            //Read everything in accept into the memory pointed to by the buf pointer
+            int readfd = read(connfd, buf, sizeof(buf));  
+            if(readfd == -1) {
+                printf("Using function read error...\n");
+                return -1;
+            }            
+            //Get the file size (copy the first n characters of the string)                
+            strncpy(file_len, buf, sizeof(file_len));                      
+            //Get the file name
+            strncpy(file_name, buf + sizeof(file_len), sizeof(file_name)); 
+            printf("Ready to receive...... file name:[%s] file size:[%s] \n", file_name, file_len);
+            //new file name 
+            //Write formatted data to a string
+            sprintf(buf, "recv-%s", file_name);                  
+            //Specify where to receive files
+            sprintf(filepath, "/home/code/tcp_download/%s", buf); 
+            //mkdir file
             int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, 0666);
-            int size = atoi(file_len); //文件大小
-            int write_len = 0;         //记录已写入的字节数
-
-            //接收文件
-            while (1)
-            {
-                bzero(buf, 1024); //清空buf内存块方便后续写入
-
-                //接收文件数据
+            //file size
+            int size = atoi(file_len); 
+            //Record the number of bytes written
+            int writen = 0;         
+            //accept
+            while (1) {
+                memset(buf,0,1024); 
                 int ret = read(connfd, buf, sizeof(buf));
-                if (ret <= 0)
-                {
-                    printf("\n [recv-%s] 文件传输完毕...\n", file_name);
+                if (ret <= 0) {
+                    printf("\n [recv-%s] trans finished...\n", file_name);
                     break;
                 }
-
-                //将数据写入文件
-                write(fd, buf, ret);
-
-                write_len += ret; //记录写入的字节数
-
-                //动态的输出接收进度
-                printf("传输中 %.2f%% \n", (float)write_len / size * 100);
+                int writefd = write(fd, buf, ret);
+                writen += ret; 
+                if(writefd == -1) {
+                    printf("Using function write error...\n");
+                    return -1;
+                }
+                //Dynamic output receive input
+                printf("Uploading %.2f%% \n", (float)writen / size * 100);
             }
-            exit(0);
+            return 0;
         }
+
         close(connfd);
     }
 }
