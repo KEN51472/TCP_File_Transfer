568fdff4e459f9d7f6bb380a2328ee1441895a58
yukuan <kenyu.99711@gmail.com>
2022-05-24 15:51:52 +0800

    修改格式

--- tcpClient/client.cpp
+++ tcpClient/client.cpp
@@ -10,10 +10,12 @@
 #define SERV_PORT 9877
 
 int main(int argc, char **argv) {
-    int connret, sock;
+    int ret, sock;
     struct sockaddr_in servaddr;
     
-    //Create a socket, initialize the socket address structure
+    /*Create a socket, initialize the socket address structure
+    On success, a file descriptor for the new socket is returned.  
+    On error, -1 is returned*/
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         printf("Create socket error...\n");
@@ -29,12 +31,14 @@ int main(int argc, char **argv) {
     return 1 if successful, and return 0 if it fails.*/
     inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr); 
     
-    //Connect to server
-    connret = connect(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
-    if (connret < 0) {
+    /*Connect to server.If the connection or binding succeeds, 
+    zero is returned. On error, -1 is returned*/
+    ret = connect(sock, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
+    if (ret < 0) {
         printf("Connect error...\n");
         return -1;
     }
+    printf("Connect to server success...\n");
 
     char file_path[128] = {0};  
     char file_info[2048] = {0}; 
@@ -46,58 +50,61 @@ int main(int argc, char **argv) {
     //Get filename from filepath
     char file_name[128] = {0};
     strncpy(file_name, basename(file_path), sizeof(file_name));
+    //On success, return the new file descriptor. On error, -1 is returned
     int fd = open(file_path, O_RDWR);
-    if (fd == 0) {
-        printf("Open [%s] success...\n",file_path);
-    }else if (fd == -1) {
+    if (fd == -1) {
         printf("Open [%s] error...\n", file_path);
-        close(fd);
         return -1;
     }
+    printf("Open [%s] success...\n",file_path);
     
     //Calculate file size
     int len = lseek(fd, 0, SEEK_END);
     //Offset the file cursor to the beginning of the file
     lseek(fd, 0, SEEK_SET);
-    if (sizeof(len) > sizeof(file_info)) {
-        printf("File is too large to transfer...\n");
-        close(fd);
-        return -1;
-    }
+    printf("file_size : %d\n",len);
     //Store file size and file name in file_info
     sprintf(file_info, "%d", len);
     strcpy(file_info + 16, file_name);
     // Tell server the name of the file to be uploaded
-    int writen = write(sock, file_info, 1024);
+    int wn = write(sock, file_info, 1024);
+    if (wn == -1) {
+        printf("Using function write error...\n");
+        close(fd);
+        return -1;
+    }
     
     int sent = 0; 
     while (1) {
         memset(buf, 0, sizeof(buf));
-        int readn = read(fd, buf, sizeof(buf));
-        if (readn == 0) {
+        //On success, the number of bytes read is returned. On error, -1 is returned
+        int rn = read(fd, buf, sizeof(buf));
+        if (rn == 0) {
             printf("Transfer [%s] success...\n", file_name);
             break;
-        }else if (readn < 0) {
+        }    
+        if (rn < 0) {
             printf("Function read error...\n");
             close(fd);
             return -1;
-        }else{
-            printf("read:%d\n",readn);
         }
-        
-        int writen = write(sock, buf, readn);
-        if (writen > 0 && sizeof(writen) < sizeof(readn)) {
+        printf("read:%d\t",rn);
+
+        //On success, the number of bytes written is returned. On error,-1 is returned        
+        int wn = write(sock, buf, rn);
+        if ((wn > 0) && (wn < rn)) {
             printf("The disk is full, or the file length limit for a given process has been exceeded...\n");
             close(fd);
             return -1;
-        }else if (writen == -1) {
+        }
+        if (wn == -1) {
             printf("Using function write error...\n");
             close(fd);
             return -1;
-        }else {
-            printf("write:%d\n",writen);
-        } 
-        sent += writen; 
+        }
+        printf("write:%d\n",wn);
+        
+        sent += wn; 
         printf("Uploading ... %.2f%%\n", (float)sent / len * 100);
     }
     
--- tcpServer/server.cpp
+++ tcpServer/server.cpp
@@ -11,7 +11,7 @@
 #define	SA struct sockaddr
 
 int main(int argc, char **argv) {
-    int sock, listenret, connret, bindret;
+    int sock, listenret, connret, ret;
     pid_t childpid;
     socklen_t clilen;
     struct sockaddr_in cliaddr, servaddr;
@@ -29,15 +29,17 @@ int main(int argc, char **argv) {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(SERV_PORT);
     
-    bindret = bind((sock), (SA *)&servaddr, sizeof(servaddr)); 
-    if (bindret < 0) {
+    //On success, zero is returned.  On error, -1 is returned
+    ret = bind((sock), (SA *)&servaddr, sizeof(servaddr)); 
+    if (ret == -1) {
         printf("Bind error...\n");
         return -1;
     }
     printf("Binding the port success...\n");
 
+    // On success, zero is returned.  On error, -1 is returned
     listenret = listen(sock, LISTENQ); 
-    if (listenret < 0) {
+    if (listenret == -1) {
         printf("Listening error...\n");
         return -1;
     }
@@ -45,12 +47,11 @@ int main(int argc, char **argv) {
 
     printf("Waiting for client connection to complete...\n");
     for (;;) { 
-        //waiting for connect
         clilen = sizeof(cliaddr);
-        
-        //accept success 
+        /*On success, these system calls return a file descriptor for the
+        accepted socket. On error, -1 is returned.*/
         connret = accept(sock, (SA *)&cliaddr, &clilen); 
-        if (connret < 0) {
+        if (connret == -1) {
             printf("Accept error...\n");
             return -1;
         }
@@ -61,15 +62,13 @@ int main(int argc, char **argv) {
             close(listenret);
             char file_len[16] = {0};   
             char file_name[128] = {0}; 
-            //data buffer
             char buf[1024] = {0};      
             char filepath[2048] = {0};
            
-            //Read file size and file name
-            //Read everything in accept into the memory pointed to by the buf pointer
             int readn = read(connret, buf, sizeof(buf));  
             if (readn == -1) {
                 printf("Using function read error...\n");
+                close(connret);
                 return -1;
             }            
             //Get the file size (copy the first n characters of the string)                
@@ -79,43 +78,51 @@ int main(int argc, char **argv) {
             printf("Ready to receive...... file name:[%s] file size:[%s] \n", file_name, file_len);
             
             //new file name 
-            //Write formatted data to a string
             sprintf(buf, "recv-%s", file_name);                  
             //Specify where to receive files
             sprintf(filepath, "/home/code/tcp_download/%s", buf); 
             //mkdir file
             int fd = open(filepath, O_RDWR | O_CREAT | O_TRUNC, 0666);
+            if (fd == -1) {
+                printf("Open error...\n");
+                close(connret);
+                return -1;
+            }
             //file size
-            int size = atoi(file_len); 
+            int size = atoi(file_len);            
             
-            //Record the number of bytes written
             int received = 0;         
-            //accept
             while (1) {
                 memset(buf,0,1024); 
-                int readn = read(connret, buf, sizeof(buf));
-                if (readn == 0) {
+                int rn = read(connret, buf, sizeof(buf));
+                if (rn == 0) {
                     printf("Transfer [%s] success...\n", file_name);
                     break;
-                }else if (readn < 0) {
+                }
+                if (rn < 0) {
                     printf("Function read error...\n");
+                    close(fd);
+                    close(connret);
                     return -1;
                 }
 
-                int writen = write(fd, buf, readn);
-                if ((writen > 0)&& (sizeof(writen) < sizeof(readn))) {
-                    printf("The disk is full, "
-                    "or the file length limit for a given process has been exceeded.\n");
+                int wn = write(fd, buf, rn);
+                if ((wn > 0)&& (wn < rn)) {
+                    printf("The disk is full, or the file length limit for a given process has been exceeded.\n");
+                    close(fd);
+                    close(connret);
                     return -1;
-                }else if (writen == -1) {
+                }
+                if (wn == -1) {
                     printf("Using function write error...\n");
                     close(fd);
+                    close(connret);
                     return -1;
                 } 
-                received += writen; 
-                //Dynamic output receive input
+                received += wn; 
                 printf("Uploading %.2f%% \n", (float)received / size * 100);
             }
+            close(fd);
             return 0;
         }
 
