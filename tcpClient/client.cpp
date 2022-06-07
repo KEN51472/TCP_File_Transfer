5ca3de0f8c036d454d69b00bfa5cd49f45bb7474
yukuan <kenyu.99711@gmail.com>
2022-06-07 09:50:50 +0800

    完善构造函数

--- tcpClient/client.cpp
+++ tcpClient/client.cpp
@@ -7,118 +7,91 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <wrapper.h>
 
 #define SERV_PORT 9877
 
-class Socket
-{
-public:
-    int sock;
-    Socket() {
-        sock = socket(AF_INET, SOCK_STREAM, 0);
-    } 
-    ~Socket() {
-        if(sock > 0) {
-            close(sock);
-            printf("socket closed success...\n");
-        }     
-    };
-};
-
-class ReadFile
-{
-public:
-    int fd;  
-    ReadFile(char *file_path) {
-        fd = open(file_path, O_RDWR);
-    } 
-    ~ReadFile() {
-        if (fd > 0) {
-            close(fd);
-            printf("fd closed success...\n");
-        }
-    };
-};
-
 int main(int argc, char **argv) {
-    int ret;
+    int ret, sock, fd;
     struct sockaddr_in servaddr;
 
-    Socket socket;
-    if (socket.sock == -1) {
-        printf("Create socket error...\nerrno is: %d\n", errno);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    DesWrapper socket_wrapper(sock);
+    if (socket_wrapper.get() == -1) {
+        cout << "Create socket error...\t" << "errno : " << errno << endl;
         return -1;
     }
-    printf("Create socket success...\n");
+    cout << "Create socket success..." << endl;
     
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
     inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr); 
     
-    ret = connect(socket.sock, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
+    ret = connect(socket_wrapper.get(), (struct sockaddr *)&servaddr, sizeof(servaddr)); 
     if (ret < 0) {
-        printf("Connect error...\nerrno is: %d\n", errno);
+        cout << "Connect error...\t" << "errno : " << errno << endl;
         return -1;
     }
-    printf("Connect to server success...\n");
+    cout << "Connect to server success..." << endl;
     
     char file_info[2048] = {0}; 
     char buf[65536] = {0};
     char file_path[128] = {0};
-    printf("Please enter the path of the file to be transferred: ");
+    cout << "Please enter the path of the file to be transferred: ";
     scanf("%s", file_path); 
-    ReadFile rf(file_path); 
+    fd = open(file_path, O_RDWR);
+    DesWrapper fd_wrapper(fd);
     char file_name[128] = {0};
     strncpy(file_name, basename(file_path), sizeof(file_name));
-    if (rf.fd == -1) {
-        printf("Open [%s] error...\nerrno is: %d\n", file_path, errno);
+    if (fd_wrapper.get() == -1) {
+        cout << "Open [" << file_path << "] error...\t" << "errno : " << errno << endl;
         return -1;
     }
-    printf("Open [%s] success...\n", file_path);
+    cout << "Open [" << file_path << "] success..." << endl;
     
-    int len = lseek(rf.fd, 0, SEEK_END);
-    lseek(rf.fd, 0, SEEK_SET);
-    printf("file_size : %d\n", len);
+    int len = lseek(fd_wrapper.get(), 0, SEEK_END);
+    lseek(fd, 0, SEEK_SET);
+    cout << "file_size : " << len << endl;
     //Store file size and file name in file_info
     sprintf(file_info, "%d", len);
     strcpy(file_info + 16, file_name);
     // Tell server the name of the file to be uploaded
-    int writeinfo = write(socket.sock, file_info, 1024);
+    int writeinfo = write(socket_wrapper.get(), file_info, 1024);
     if (writeinfo == -1) {
-        printf("Using function write error...\nerrno is: %d\n", errno);
+        cout << "Using function write error...\t" << "errno : " << errno << endl;
         return -1;
     }
     
     int sent = 0; 
     while (1) {
         memset(buf, 0, sizeof(buf));
-        int rn = read(rf.fd, buf, sizeof(buf));
+        int rn = read(fd_wrapper.get(), buf, sizeof(buf));
         if (rn == 0) {
-            printf("Transfer [%s] success...\n", file_name);
+            cout << "Transfer [" << file_name << "] success..." << endl;
             break;
         }    
         if (rn < 0) {
-            printf("Function read error...\nerrno is: %d\n", errno);
+            cout << "Function read error...\t" << "errno : " << errno << endl;
             return -1;
         }
         
         int left = rn;   
         while (left > 0) {
-            int wn = write(socket.sock, buf, left);          
+            int wn = write(socket_wrapper.get(), buf, left);          
             if (wn == -1) {
-                printf("Using function write error...\nerrno is: %d\n", errno);
+                cout << "Using function write error...\t" << "errno : " << errno << endl;
                 return -1;
             } 
             left -= wn;
             sent += wn;
-            printf("sent:%d\n",sent);
+            cout << "sent :" << sent << endl;
             if (left == 0) {
                 break;
             }
-            printf("missing write size :%d\nrewrite:%d\n", left, wn); 
+            cout << "missing write size :" << left <<"\trewrite:" << wn << endl;  
         }    
-        printf("Uploading ... %.2f%%\n", (float)sent / len * 100);
+        cout << "Uploading ..." << (float)sent /len * 100 << "%" << endl;
     }
     return 0;
 }
